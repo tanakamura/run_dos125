dac5c107f60654a13dd6b332b9dc0a60dc7eb058
name <mail>
2024-08-14 00:55:32 +0900

    tmp

--- vm.cc
+++ vm.cc
@@ -14,6 +14,10 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
+#include <mutex>
+#include <queue>
+#include <thread>
+
 #if 0
 #define NUM_SECTOR 8
 #define NUM_HEAD 2
@@ -29,6 +33,8 @@
 bool debug = false;
 uint16_t dos21_seg = 0;
 uint16_t dos21_offset = 0;
+std::queue<char> key_queue;
+std::mutex q_lock;
 
 struct vcpu {
     char *run;
@@ -145,6 +151,12 @@ static void dump_ivt(unsigned char *full_mem) {
                *(unsigned short *)&full_mem[i * 4 + 0],
                *(unsigned short *)&full_mem[i * 4 + 2]);
     }
+
+    for (int i = 0; i < 0x100; i++) {
+        printf("handler [%02x] = %x %x %x %x\n", i, full_mem[0xf0000 + i + 0],
+               full_mem[0xf0000 + i + 1], full_mem[0xf0000 + i + 2],
+               full_mem[0xf0000 + i + 3]);
+    }
 }
 
 static void handle_hlt(int fd, unsigned char *full_mem, int image_fd) {
@@ -182,6 +194,11 @@ static void handle_hlt(int fd, unsigned char *full_mem, int image_fd) {
                         putchar(regs.rax & 0xff);
                         fflush(stdout);
                         break;
+                    case 1:  // set cursor shape
+                        break;
+                    case 0xf:  // get current video mode
+                        cf = 1;
+                        break;
                     default:
                         printf("video intr %x\n", (regs.rax) >> 8);
                         break;
@@ -194,7 +211,7 @@ static void handle_hlt(int fd, unsigned char *full_mem, int image_fd) {
                 break;
 
             case 0x12:  // get memory size
-                regs.rax = 1024 * 256;
+                regs.rax = 256;
                 break;
 
             case 0x13:
@@ -209,7 +226,8 @@ static void handle_hlt(int fd, unsigned char *full_mem, int image_fd) {
                         break;  // reset
                     }
 
-                    case 2: {
+                    case 2:
+                    case 3: {
                         int num_sector = regs.rax & 0xff;
                         int cyl = (regs.rcx >> 8) |
                                   (((regs.rcx & 0xc0) << 2) & 0x300);
@@ -232,8 +250,13 @@ static void handle_hlt(int fd, unsigned char *full_mem, int image_fd) {
                             //    " "drive=%d %x\n", lba, num_sector, cyl,
                             //    sector, head, drive, buffer);
 
-                            pread(image_fd, full_mem + buffer, num_sector * 512,
-                                  lba * 512);
+                            if ((regs.rax >> 8 &0xff) == 2) {
+                                pread(image_fd, full_mem + buffer, num_sector * 512,
+                                      lba * 512);
+                            } else {
+                                pwrite(image_fd, full_mem + buffer, num_sector * 512,
+                                       lba * 512);
+                            }
                             regs.rax = num_sector;
                         }
                         break;
@@ -278,25 +301,38 @@ static void handle_hlt(int fd, unsigned char *full_mem, int image_fd) {
                 break;
 
             case 0x16:
-                // debug = 1;
-
-                if (*(uint16_t *)&full_mem[0x21 * 4 + 2] != 0xf000) {
-                    dos21_offset = *(uint16_t *)&full_mem[0x21 * 4 + 0];
-                    dos21_seg = *(uint16_t *)&full_mem[0x21 * 4 + 2];
-                    *(uint16_t *)&full_mem[0x21 * 4 + 0] = 0x21;
-                    *(uint16_t *)&full_mem[0x21 * 4 + 2] = 0xf000;
-                    puts("replace dos21");
-                }
-
                 switch (regs.rax >> 8) {
                     case 0x00:
-                    case 0x10:
-                        zf = 1;
-                        break;
+                    case 0x10: {
+                        std::lock_guard lk(q_lock);
+                        if (key_queue.empty()) {
+                            zf = 1;
+                        } else {
+                            zf = 0;
+                            char c = key_queue.front();
+                            key_queue.pop();
+                            if (c == '\n') {
+                                regs.rax = '\r';
+                            } else {
+                                regs.rax = c;
+                            }
+                        }
+                    } break;
                     case 0x01:
-                    case 0x11:
-                        zf = 1;
-                        break;
+                    case 0x11: {
+                        std::lock_guard lk(q_lock);
+                        if (key_queue.empty()) {
+                            zf = 1;
+                        } else {
+                            zf = 0;
+                            char c = key_queue.front();
+                            if (c == '\n') {
+                                regs.rax = '\r';
+                            } else {
+                                regs.rax = c;
+                            }
+                        }
+                    } break;
                     default:
                         printf("unknown 0x16 %x", regs.rax >> 8);
                         zf = 1;
@@ -345,6 +381,7 @@ static void handle_hlt(int fd, unsigned char *full_mem, int image_fd) {
             } break;
 
             case 0x21: {
+                // debug = true;
                 dos = true;
                 sregs.cs.base = dos21_seg * 16;
                 sregs.cs.selector = dos21_seg;
@@ -354,9 +391,9 @@ static void handle_hlt(int fd, unsigned char *full_mem, int image_fd) {
             }
 
             default:
-                printf("unknown intr 0x%x, prev_ip=%04x, prev_cs=%04x%x\n",
-                       intr_nr, prev_ip, prev_cs);
-                // dump_regs(fd);
+                // printf("unknown intr 0x%x, prev_ip=%04x, prev_cs=%04x%x\n",
+                // intr_nr, prev_ip, prev_cs);
+                //  dump_regs(fd);
                 cf = 1;
                 break;
         }
@@ -405,8 +442,8 @@ static void disasm(int fd, unsigned char *full_mem) {
     struct kvm_regs regs;
     ioctl(fd, KVM_GET_REGS, &regs, NULL);
 
-    printf("rip=%x, cs=%x, ds=%x flags=%08x\n", regs.rip, sregs.cs.base,
-           sregs.ds.base, regs.rflags);
+    printf("rip=%x, cs=%x, ds=%x ss=%x flags=%08x\n", regs.rip, sregs.cs.base,
+           sregs.ds.base, sregs.ss.base, regs.rflags);
 
     unsigned char *code = &full_mem[regs.rip + sregs.cs.base];
     if (code[0] == 0xf3 && code[1] == 0xa4) {
@@ -420,10 +457,10 @@ static void disasm(int fd, unsigned char *full_mem) {
         fwrite(&full_mem[regs.rip + sregs.cs.base], 1, 16, fp);
         fclose(fp);
 
-        int r = system("ndisasm out.bin|head -n2");
-        if (r != 0) {
-            exit(1);
-        }
+        // int r = system("ndisasm out.bin|head -n2");
+        // if (r != 0) {
+        //     exit(1);
+        // }
     }
     fflush(stdout);
 }
@@ -447,7 +484,7 @@ int main(int argc, char **argv) {
     // for (int i = 0; i < 1024 * 1024 / 4; i++) {
     //     ((unsigned int *)full_mem)[i] = i;
     // }
-    memset(full_mem, 0x00, 1024 * 1024);
+    memset(full_mem, 0xf4, 1024 * 1024);
     memset(full_mem + 0x400, 0x88, 256);  // bda
     int num_int = 256;
 
@@ -460,7 +497,7 @@ int main(int argc, char **argv) {
 
     // int image_fd = open("FD13BOOT.img", O_RDONLY);
     //  int image_fd = open("dos11src.img", O_RDONLY);
-    int image_fd = open("floppy", O_RDONLY);
+    int image_fd = open("floppy", O_RDWR);
     if (image_fd < 0) {
         perror("unable to open a");
         return 1;
@@ -491,18 +528,40 @@ int main(int argc, char **argv) {
     mem.slot = 0;
     mem.flags = 0;
     mem.guest_phys_addr = 0;
-    mem.memory_size = 1024 * 1024;
+    mem.memory_size = (1024 - 64) * 1024;
     mem.userspace_addr = (__u64)full_mem;
-
     int r = ioctl(vm_fd, KVM_SET_USER_MEMORY_REGION, &mem, NULL);
 
+    mem.slot = 1;
+    mem.flags = KVM_MEM_READONLY;
+    mem.guest_phys_addr = 0xf0000;
+    mem.memory_size = 64 * 1024;
+    mem.userspace_addr = (__u64)(full_mem + 0xf0000);
+    r = ioctl(vm_fd, KVM_SET_USER_MEMORY_REGION, &mem, NULL);
+    if (r < 0) {
+        perror("map");
+        exit(1);
+    }
+
     setup_vcpu(vcpu_fd, 0x7c00);
-    signal(SIGINT, sigint);
+    // signal(SIGINT, sigint);
 
     bool done = false;
     struct kvm_guest_debug single_step = {};
     single_step.control = KVM_GUESTDBG_ENABLE | KVM_GUESTDBG_SINGLESTEP;
     // ioctl(vcpu_fd, KVM_SET_GUEST_DEBUG, &single_step, NULL);
+
+    std::thread tK([&q_lock, &key_queue]() {
+        while (1) {
+            int c = getchar();
+            if (c == EOF) {
+                break;
+            }
+
+            std::lock_guard lk(q_lock);
+            key_queue.push(c);
+        }
+    });
 
     while (!done) {
         if (debug) {
@@ -519,6 +578,7 @@ int main(int argc, char **argv) {
                 }
                 printf("]\n");
                 dump_regs(vcpu_fd);
+                dump_ivt(full_mem);
                 done = 1;
                 break;
 
@@ -526,6 +586,8 @@ int main(int argc, char **argv) {
                 printf("reference unmapped region : addr=%16llx\n",
                        (long long)run->mmio.phys_addr);
                 dump_regs(vcpu_fd);
+                disasm(vcpu_fd, full_mem);
+                dump_ivt(full_mem);
                 done = 1;
                 break;
 
